652ea350e9a58660e37087821f73bb6ba3248b33
Revathy Ramalingam <revathy.ramalingam@gmail.com>
2026-01-05 11:39:29 +0000

    added pytest

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,9 +1,11 @@
 import pytest
+import pandas as pd
+import src.train as train
 
-def test_load_dataset(tmp_path, monkeypatch):
+def test_load_dataset1(tmp_path, monkeypatch):
     #create a mock csv file
     df = pd.DataFrame({"Unnamed: 0":[1,2],
-    "Packet loss %": [1,2,3],
+    "Packet loss %": [1,2],
     "Avg Delay (ms)":[1,2]
     })
 
@@ -18,3 +20,21 @@ def test_load_dataset(tmp_path, monkeypatch):
 
     assert "packet_loss_pct" in result.columns
     assert "avg_delay_ms" in result.columns
+
+def test_load_dataset2(tmp_path, monkeypatch):
+    #create a mock csv file
+    df = pd.DataFrame({"valid_column_name":[1,2],
+    "SOME_COLUMN":[1,2]
+    })
+
+    csv_path = tmp_path / "test.csv"
+    df.to_csv(csv_path, index=False)
+
+    fake_file = tmp_path / "train.py"
+    fake_file.touch()
+
+    monkeypatch.setattr(train,"__file__", str(fake_file))
+    result = train.load_dataset(csv_path)
+
+    assert "valid_column_name" in result.columns
+    assert "some_column" in result.columns
